3c7720bc7201f4df42e329022a9fa25171c57718
troydelageto <leroyroy033@gmail.com>
2022-11-09 17:18:54 +0100

    created the comment.js,post.js,user.js,postedBy.js schema in sanity-backend/schemas

---
+++ sanity-backend/schemas/post.js
@@ -0,0 +1,48 @@
+export default{
+    name:'post',
+    title:'Post',
+    type:'document',
+    fields:[
+        {
+        name:'caption',
+        title:'Caption',
+        type:'string'
+
+         },
+         {
+            name:'video',
+            title:'Video',
+            type:'file',
+            options:{
+                hotspot:true
+            }
+         },
+         {
+            name:'userId',
+            title:'User Id',
+            type:'string'
+         },
+         {
+            name:'postedBy',
+            title:'PostedBY',
+            type:'array',
+            of:[
+                {
+                    type:'reference',
+                    to:[{type:user}]
+                }
+            ]
+         },
+         {
+            name:'comments',
+            title:'Comments',
+            type:'array',
+            of :[{type:'comment'}]
+         },
+         {
+            name:'topic',
+            title:'Topic',
+            type:'string'
+         }
+] 
+}
